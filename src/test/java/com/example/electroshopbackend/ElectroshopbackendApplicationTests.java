f1499d1c19fc2364c26a8f5b1565d48465f0f198
Sinisa Gromovic <gromso97@gmail.com>
2023-09-21 12:24:40 +0200

    Initial commit

---
+++ src/test/java/com/example/electroshopbackend/ElectroshopbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electroshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroshopbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
